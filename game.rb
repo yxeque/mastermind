4f370824ccdef4ee7dfc8d9a6391e9a0b8b9bc0e
yxeque <yxequel@gmail.com>
2024-01-31 21:06:31 +0800

    Fix main game logic

--- game.rb
+++ game.rb
@@ -9,6 +9,10 @@ class Code
     new(COLORS.sample(4))
   end
 
+  def colors
+    @colors
+  end
+
   def compare(player_code)
     black_pegs = 0
     white_pegs = 0
@@ -67,30 +71,27 @@ class Player
 end
 
 secret_code = Code.generate_random
+puts secret_code
+
 current_player = Player.new("guesser")
 turn_number = 1
 is_won = false
 
 while !is_won && turn_number <= 10
+
   guess = current_player.make_guess
   feedback = Feedback.calculate(secret_code, guess)
   puts "Feedback: #{feedback.black_pegs} black pegs, #{feedback.white_pegs} white pegs"
 
   if feedback.black_pegs == 4
-    puts "Congratulations, you guessed the code!"
+    puts "I cracked your code! The secret code was #{secret_code}."
     is_won = true
   else
     turn_number += 1
-    current_player = current_player.opponent
   end
 end
 
 if !is_won
-  puts "Sorry, you couldn't guess the code. The secret code was: #{secret_code}"
+  puts "You successfully kept your code secret! Congratulations!"
 end
 
-secret_code = Code.new(["R", "B", "O", "Y"])    # Create a test code manually
-guess_code = Code.new(['Y', 'B', 'R', 'O'])
-
-feedback = Feedback.calculate(secret_code, guess_code)
-puts "Feedback: #{feedback.black_pegs} black pegs, #{feedback.white_pegs} white pegs"
